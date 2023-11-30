76e11d3850d9367c5728c40dc29bd95a17cf1b20
wny0320 <33829442+wny0320@users.noreply.github.com>
2023-11-30 20:58:47 +0900

    appScene 사람 모델링 추가

--- Project GG/Assets/Scripts/Health/Exercise.cs
+++ Project GG/Assets/Scripts/Health/Exercise.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="Exercise")]
 public class Exercise : ScriptableObject
 {
-    public string title;
-    public BodyParts part;
-    public int times;
-    public int exp;
-    [TextArea] public string description;
+    public string title; // 운동의 한국어 이름
+    public BodyParts part; // 운동의 부위
+    public int times; // 운동의 횟수
+    public int exp; // 운동의 경험치량
+    [TextArea] public string description; // 운동의 설명
 }
